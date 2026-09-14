9602194304d8a708f6568ca9df8aebd04794fe06
tobiasaobregon-hue <tobiasaobregon@gmail.com>
2026-09-14 19:11:20 -0300

    Update database connection parameters

--- includes/conexion.php
+++ includes/conexion.php
@@ -2,10 +2,10 @@
 
 $servidor = "sql200.infinityfree.com";
 $usuario = "if0_42918116";
-$password = "dHk3GpHhEMKQ7B";
-$base_datos = "if0_42918116_XXX";
+$password = "TU_CONTRASEÑA";
+$base_datos = "if0_42918116_graft_point";
 
-$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos, 3306);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
