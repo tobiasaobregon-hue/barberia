2adfaa6b96454037a1edb5696e4d1fb04c5981a4
tobiasaobregon-hue <tobiasaobregon@gmail.com>
2026-09-14 19:08:16 -0300

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,16 +1,11 @@
 <?php
 
-$servidor = "localhost";
-$usuario = "root";
-$password = "";
-$base_datos = "graft_point";
+$servidor = "sql200.infinityfree.com";
+$usuario = "if0_42918116";
+$password = "dHk3GpHhEMKQ7B";
+$base_datos = "if0_42918116_XXX";
 
-$conexion = new mysqli(
-    $servidor,
-    $usuario,
-    $password,
-    $base_datos
-);
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
